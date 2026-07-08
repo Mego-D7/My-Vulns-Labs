fcaf0707d48249493c375ad68a595a77946e7faa
Magdy_Ahmed <my.freefire2007@gmail.com>
2026-07-08 10:45:16 -0700

    Add files via upload

---
+++ Prototype-Pollution/Analyzing_JS_PP/!/searchLoggerAlternative.js
@@ -0,0 +1,25 @@
+async function logQuery(url, params) {
+    try {
+        await fetch(url, {method: "post", keepalive: true, body: JSON.stringify(params)});
+    } catch(e) {
+        console.error("Failed storing query");
+    }
+}
+
+async function searchLogger() {
+    window.macros = {};
+    window.manager = {params: $.parseParams(new URL(location)), macro(property) {
+            if (window.macros.hasOwnProperty(property))
+                return macros[property]
+        }};
+    let a = manager.sequence || 1;
+    manager.sequence = a + 1;
+
+    eval('if(manager && manager.sequence){ manager.macro('+manager.sequence+') }');
+
+    if(manager.params && manager.params.search) {
+        await logQuery('/logger', manager.params);
+    }
+}
+
+window.addEventListener("load", searchLogger);
